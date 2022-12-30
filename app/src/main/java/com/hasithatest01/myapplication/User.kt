cfad5bb5580a6f8073f490ecc1ffb04e3f89b92c
Hasitha-Su <hasithakms@gmail.com>
2022-12-30 10:26:38 +0530

    Initial commit

---
+++ app/src/main/java/com/hasithatest01/myapplication/User.kt
@@ -0,0 +1,20 @@
+/**********************************************************************************************************
+ * @author : Hasitha Subhashana
+ * @Description  :
+ * ========================================================================================================
+ * @History
+ * --------------------------------------------------------------------------------------------------------
+ * VERSION     AUTHOR                    DATE         DETAIL
+    1.0    Hasitha Subhashana       23/11/2022   Initial implementation
+ **********************************************************************************************************/
+package com.hasithatest01.myapplication
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val imagePath: String,
+) : Parcelable
